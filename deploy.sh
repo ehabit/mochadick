f50550fbdc152bff858e5223814d257339cfbf9d
Glen Baker <iepathos@gmail.com>
2015-03-18 05:06:30 -0400

    Updated deploy commit message

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 hugo -t casper
 git add .
-git commit -m 'Updated site content'
+git commit -m 'Updated site, regenerated content.'
 git push --force
 git subtree push --prefix=public git@github.com:ehabit/mochadick_web.git gh-pages
