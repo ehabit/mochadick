bb2fcd7059e7109503fd6f2c428303ca57c18c4b
Glen Baker <iepathos@gmail.com>
2015-03-18 20:04:20 -0400

    Updated site, regenerated content.

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ hugo -t casper
 git add .
 git commit -m 'Updated site, regenerated content.'
 git push origin master --force
-git subtree push --prefix=public git@github.com:ehabit/mochadick_web.git gh-pages
+git subtree push --prefix=public git@github.com:ehabit/mochadick.git gh-pages
